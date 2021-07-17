8db4b6e955e75ab42c2054a967524478dea2f64b
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-17 11:57:30 -0700

    Initial Commit

---
+++ Delegation/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegation
+//
+//  Created by Kenneth Dubroff on 7/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Let's Go On Vacation!"
+    }
+
+
+}
+

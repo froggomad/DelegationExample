114fed3a8447fea51fcfd97b27020744b573c80c
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-17 13:51:32 -0700

    using `viewDidLoad` to setup "done" `barButtonItem`

--- Delegation/Controller/PickingViewController.swift
+++ Delegation/Controller/PickingViewController.swift
@@ -11,8 +11,20 @@ class PickingViewController: UIViewController {
     
     private lazy var pickingView: PickingView = PickingView()
     
+    override func viewDidLoad() {
+        let action = UIAction(handler: { [unowned self] _ in
+            self.done()
+        })
+        let barbuttonItem = UIBarButtonItem(systemItem: .done, primaryAction: action)
+        navigationItem.rightBarButtonItem = barbuttonItem
+    }
+    
     override func loadView() {
         super.loadView()
         view = pickingView
+    }
+    
+    @objc func done() {
+        navigationController?.popViewController(animated: true)
     }
 }

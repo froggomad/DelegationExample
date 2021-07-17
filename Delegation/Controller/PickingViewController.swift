a0ebd3a1d7e6c1961c3aa74066d1648217530114
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-07-17 12:45:33 -0700

    Created shell for `PickingViewController`

---
+++ Delegation/Controller/PickingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PickingViewController.swift
+//  Delegation
+//
+//  Created by Kenneth Dubroff on 7/17/21.
+//
+
+import UIKit
+
+class PickingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
